c3573d3a9bece0defeccf64479658a4049453b1d
A-Coder-Named-Ryan <github.uj6y1@slmails.com>
2026-09-24 04:01:02 +1000

    going nucular

--- Source/Lib/Globals/enc_settings.c
+++ Source/Lib/Globals/enc_settings.c
@@ -740,23 +740,6 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet* scs) {
             "encoding modes.\n");
         return_error = EB_ErrorBadParameter;
     }
-    // Block the use of M-1 or lower for resolutions higher than 4K, unless allintra coding is used (due to memory constraints)
-    if (!scs->allintra && (uint64_t)(scs->max_input_luma_width * scs->max_input_luma_height) > INPUT_SIZE_4K_TH &&
-        config->enc_mode <= ENC_M4) {
-        if (config->enc_mode >= ENC_M2) {
-            SVT_WARN(
-                "8K+ resolution support below M5 isn't officially supported. 64 GB of available memory is "
-                "recommended.\n");
-        } else if (config->enc_mode >= ENC_M-1) {
-            SVT_WARN(
-                "8K+ resolution support below M2 is experimental. More then 64 GB of available memory is "
-				"recommended.\n");
-        } else {
-            // M-1 (Research) and below
-            SVT_ERROR("8K+ resolution support is limited to M-1 and faster presets.\n");
-            return_error = EB_ErrorBadParameter;
-        }
-    }
     if (config->pass > 0 && scs->static_config.enable_overlays) {
         SVT_ERROR(
             "The overlay frames feature is currently not supported with multi-pass "
